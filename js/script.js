b4ae17c7ea3cdf4c0350fdac9b0a4e78f26dc851
Flopiras <Flopiras@yahoo.it>
2023-06-08 15:59:03 +0200

    add new step

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ console.log('js ok');
 // |----------|
 
 //generare cella
-function createCell(){
+function createCell(cellAmount){
     const cell = document.createElement('div');
     cell.className = 'cell'; 
 
@@ -16,32 +16,45 @@ function createCell(){
 //elementi del DOM
 const grid = document.getElementById('grid');
 const button = document.getElementById('button-play');
+const scoreDisplay = document.getElementById('score');
+const scoreCard = document.getElementById('score-card');
+
+
 
 //bottone in ascolto
 button.addEventListener('click', () => {
-
+    //appare la card per il punteggio
+    scoreCard.classList.remove('d-none')
+    
     const cols = 10;
     const rows = 10;
     const cellAmount = rows * cols;
     
     //creare 100 celle
+    
     for(let i = 0; i < cellAmount; i++){
         const cell = createCell();
-
-    //preparare il collegamento per l'html
-    grid.appendChild(cell);
-
-    //mettere la cella in ascolto
-    cell.addEventListener('click', function(){
+        
+        //preparare il collegamento per l'html
+        grid.appendChild(cell);
+        
+        
+        //mettere la cella in ascolto
     
-        //inserire il numero all'interno delle celle
-        cell.classList.add('selected');
-        const cellNumber = [i + 1];
-        cell.innerText = cellNumber;
+        cell.addEventListener('click', function(){
+            let isSelected;
+            if(!isSelected){
+            //inserire il numero all'interno delle celle
+            cell.classList.add('selected');
+            const cellNumber = [++i];
+            cell.innerText = cellNumber;
+            console.log(i)
+            }
 
-    })
-    }
-    
+        })
+
+    }  
+       
     
 })
 
